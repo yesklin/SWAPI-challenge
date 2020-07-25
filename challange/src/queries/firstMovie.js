6c4801d4ce9cd71e690c98d818106531f869d2dd
yesklin <cd.santos.360@gmail.com>
2020-07-25 16:18:16 -0300

    removing static function

--- challange/src/queries/firstMovie.js
+++ challange/src/queries/firstMovie.js
@@ -4,12 +4,12 @@ const Parse = require('parse/node');
 
 class FirstMovie{
 
-  static request = async () => {
+  request = async () => {
     const Film = Parse.Object.extend("Film"); //extending class
     const query = new Parse.Query(Film); // creating query
     query.select("title", "releaseDate").ascending(); //constrains
     const response = await query.find();
-    const firstMovie = await response[0];
+    const firstMovie = response[0];
     return firstMovie;
   }
 
