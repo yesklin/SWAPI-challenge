704d2f73d14bd774bf7d3b960e6be7068ffe9784
yesklin <cd.santos.360@gmail.com>
2020-07-27 20:56:25 -0300

    added sorting to query

--- src/queries/firstMovie.js
+++ src/queries/firstMovie.js
@@ -4,7 +4,7 @@
 const firstMovieQuery = async (Parse) => {
   const Film = Parse.Object.extend("Film"); //extending class
   const query = new Parse.Query(Film); // creating query
-  query.select("title", "releaseDate").ascending(); //constrains
+  query.select("title", "releaseDate").ascending('releaseDate'); //constrains
   const response = await query.find();
   const firstMovie = response[0];
   return firstMovie.get('title');
